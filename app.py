af890a733300f066fa8be4bf6503cd9baabbd90f
OpsecGuy <30886720+OpsecGuy@users.noreply.github.com>
2024-10-20 21:36:00 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,104 @@
+from database import DatabaseController
+from helper import *
+
+
+logging.basicConfig(filename="application.log", level=logging.INFO, format='%(asctime)s - %(message)s') # Set up logging
+
+cfg = Config()
+
+class UDPServerProtocol(asyncio.DatagramProtocol):
+    def __init__(self, port):
+        super().__init__()
+        self.port = port
+        self.protocol_type = "UDP"
+        self.transport = None
+        self.last_five_packets = deque(maxlen=10)
+
+    def connection_made(self, transport):
+        self.transport = transport
+        # print(f"UDP server is ready and listening on port {self.port}")
+
+    def datagram_received(self, data, addr):
+        if data.hex() in self.last_five_packets:
+            # print("Duplicate packet found, skipping database entry.")
+            return
+        
+        self.last_five_packets.append(data.hex())
+        print(f"[{self.port}] Received data from {addr}: {data}")
+
+        try:
+            for protocol in known_payloads.get(str(self.port)).keys():
+                if data == known_payloads.get(str(self.port))[protocol]:
+                    # print(f"Match found for {protocol} | Data: {data}")
+                    break
+                protocol = "Unknown"
+
+        except Exception as err:
+            protocol = "Unknown"
+        
+        try:
+            is_botnet = is_potential_botnet(data)
+            db.add_payload_stats(protocol, self.port, self.protocol_type)
+            db.add_new_payload(addr[0], self.port, protocol, data, datetime.now().timestamp(), self.protocol_type, is_botnet)
+        except Exception as err:
+            logging.warning(f"Failed to log data. Reason: {err}")
+
+    def error_received(self, exc):
+        print(f"Error on port {self.port}: {exc}")
+
+    def connection_lost(self, exc):
+        # print(f"Connection closed on port {self.port}")
+        # logging.info(f"Connection closed on port {self.port}")
+        pass
+
+
+async def start_udp_server(port):
+    loop = asyncio.get_running_loop()
+    protocol = UDPServerProtocol(port)
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: protocol,
+        local_addr=(cfg.options["ip_addr"], port)
+    )
+    return transport, protocol
+
+
+async def main():
+    transports = []
+    protocols = []
+    try:
+        # Create UDP server endpoints for each port in the range
+        for port in range(cfg.options["start_port"], cfg.options["end_port"] + 1):
+            try:
+                transport, protocol = await start_udp_server(port)
+                transports.append(transport)
+                protocols.append(protocol)
+            except Exception as err:
+                print(f"Failed to start listener on port {port}")
+                logging.warning(f"Failed to start listener on port {port}. Reason: {err}")
+
+        print(f"UDP servers are running on ports {cfg.options["start_port"]} to {cfg.options["end_port"]}...")
+        
+        # Keep the servers running indefinitely
+        await asyncio.sleep(float('inf'))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Close all transports gracefully
+        for transport in transports:
+            transport.close()
+        print("All UDP servers have been closed.")
+
+
+if __name__ == "__main__":
+    logging.info("-=- Application is starting -=-")
+    try:
+        db = DatabaseController(cfg.options["db_ip"], cfg.options["db_name"], cfg.options["db_user"], cfg.options["db_pwd"], cfg.options["ip_addr"])
+        logging.info(f"Connected to database {cfg.options["db_name"]} as {cfg.options["db_user"]}")
+    except Exception as err:
+        logging.error(f"Failed to establish database connection! Reason: {err}")
+        os._exit(1)
+    
+    logging.info(f"{cfg.options["ip_addr"]} is listening on ports {cfg.options["start_port"]}:{cfg.options["end_port"]}")
+    asyncio.run(main())
+
+
---
+++ database.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+
+class DatabaseController:
+    def __init__(self, host: str, database: str, login: str, password: str, server: str):
+        self.server = server
+        
+        self.conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=login,
+            password=password
+        )
+        self.cur = self.conn.cursor()
+
+    def add_new_payload(self, ipaddr: str, port: str, protocol: str, payload: bytes, creation_date: str, protocol_type: str, is_botnet: int):
+        sql_query = "INSERT INTO data (id, ipaddr, port, protocol, payload, server, creation_date, protocol_type, is_botnet) VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM data), %s, %s, %s, %s, %s, %s, %s, %s);"
+        data = (ipaddr, port, protocol, payload.hex(), self.server, creation_date, protocol_type, is_botnet,)
+        self.cur.execute(sql_query, data)
+        self.conn.commit()
+
+    def add_payload_stats(self, name: str, port: str, protocol_type: str):
+        sql_query = "UPDATE protocols SET count = count + 1 WHERE name = %s AND port = %s AND protocol_type = %s"
+        self.cur.execute(sql_query, (name, port, protocol_type,))
+
+        # Check if the row was updated
+        if self.cur.rowcount == 0:
+            sql_query = "INSERT INTO protocols (id, name, count, port, protocol_type) VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM protocols), %s, 1, %s, %s)"
+            self.cur.execute(sql_query, (name, port, protocol_type,))
+        self.conn.commit()
+
+    def close_connection(self):
+        self.cur.close()
+        self.conn.close()
+
